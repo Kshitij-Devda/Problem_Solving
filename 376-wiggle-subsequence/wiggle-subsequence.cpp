1c3bd7e456b74f73e95d9d8970fa4f1bbfe70fa2
Kshitij-Devda <kshitijdevda996@gmail.com>
2025-06-11 15:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (86.64%) - LeetSync

---
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int peak = 0, valley = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            // current element could be a peak
+            if (nums[i - 1] < nums[i]) {
+                peak = valley + 1;
+            }
+            // current element could be a valley
+            else if (nums[i - 1] > nums[i]) {
+                valley = peak + 1;
+            }
+        }
+        // add 1 because we didn't count the first element
+        return 1 + max(peak, valley);
+    }
+};
