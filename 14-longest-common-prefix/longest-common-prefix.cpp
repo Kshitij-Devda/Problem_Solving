446c889cef9133939447e3eec378f5b59afeed8a
Kshitij-Devda <kshitijdevda996@gmail.com>
2025-06-28 01:48:14 +0530

    Time: 4 ms (8.79%) | Memory: 17.9 MB (5.45%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-         int i=0;
-         string ans;
+        
+        int i=0;
+        string ans;
 
-         while(true){
+        while(true){
             char curr=0;
-
-            
 
             for(auto str:strs){
 
-                 if(i>=str.size()){
-                curr=0;
-                break;
-            }
+                if(i>=str.size()){
+                    curr=0;
+                    break;
+                }
                 if(curr==0){
                     curr=str[i];
                 }
-                else if(str[i]!=curr){
+                else if(curr!=str[i]){
                     curr=0;
                     break;
                 }
             }
             if(curr==0){
                 break;
+
             }
             ans.push_back(curr);
             i++;
-         }
-         return ans;
+        }
+        return ans;
 
         
     }       
