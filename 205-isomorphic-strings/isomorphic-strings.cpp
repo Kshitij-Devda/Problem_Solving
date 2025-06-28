f1a13aad29f401a16e30ad6bdce5281be983dfae
Kshitij-Devda <kshitijdevda996@gmail.com>
2025-06-28 16:35:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (80.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        int hash[256]={0};
+     bool ismap[256]={0};
 
-        int hash[256]={0};//mapped character from s to t
-        bool ismap[256]={0};//store if character of t is already mapped
-
-        for(int i=0;i<s.size();i++){
-             if(hash[s[i]]==0 && ismap[t[i]]==0){
-                hash[s[i]]=t[i];
-                ismap[t[i]]=true;
-
-             }
+     for(int i=0;i<s.size();i++){
+         if(hash[s[i]]==0 && ismap[t[i]]==0){
+            hash[s[i]]=t[i];
+            ismap[t[i]]=true;
+         }
+     }
+     for(int i=0;i<s.size();i++){
+        if(hash[s[i]]!=t[i]){
+            return false;
         }
+     }
+     return true;
 
-        for(int i=0;i<s.size();i++){
-             if(hash[s[i]]!=t[i]){
-                return false;
-             }
-        }
-        return true;
-        
     }
 };
+
+
+        
