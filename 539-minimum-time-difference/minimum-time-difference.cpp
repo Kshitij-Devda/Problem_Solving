74e555b05503835af53b2f78ac4e52170f3f8dd6
Kshitij-Devda <kshitijdevda996@gmail.com>
2025-07-02 20:35:08 +0530

    Time: 8 ms (19.68%) | Memory: 19.2 MB (12.53%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
+      int converttomin(string&s){
 
-int converttomin(string&time){
-    int hr=stoi(time.substr(0,2));
-    int mm=stoi(time.substr(3,2));
+        int hr =stoi(s.substr(0,2));
+        int mm=stoi(s.substr(3,2));
 
-    return 60*hr+mm;
-}
+        return 60*hr+mm;
+
+      }
+
     int findMinDifference(vector<string>& timePoints) {
-        vector<int>mins;
+             vector<int>mins;
 
-        for(auto time:timePoints){
-            mins.push_back(converttomin(time));
-        }
-    sort(mins.begin(),mins.end());
+             for(auto s:timePoints){
+                mins.push_back(converttomin(s));
+             }
 
-    int ans=INT_MAX;
+             sort(mins.begin(),mins.end());
 
-    for( int i=0;i<mins.size()-1;i++){
-        ans=min(ans,mins[i+1]-mins[i]);
+             int ans=INT_MAX;
 
-    }
+             for(int i=0;i<mins.size()-1;i++){
+                ans=min(ans,mins[i+1]-mins[i]);
 
-    //last diff
-    int lastdiff=mins[0]+1440-mins[mins.size()-1];
-    ans=min(lastdiff,ans);
-    return ans;
+
+
+             }
+
+             //last diff
+             int lastdiff=mins[0]+1440-mins[mins.size()-1];
+             ans=min(ans,lastdiff);
+
+             return ans;
+
         
     }
 };
