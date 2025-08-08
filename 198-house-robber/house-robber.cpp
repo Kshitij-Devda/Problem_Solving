01a872c67d5b31e83938b2eb359fed97b23fb837
Kshitij-Devda <kshitijdevda996@gmail.com>
2025-08-08 15:46:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (64.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
 int solve(vector<int>& nums,int i,int n,vector<int>&t){
     if(i>=n)return 0;
@@ -13,6 +13,41 @@ int solve(vector<int>& nums,int i,int n,vector<int>&t){
         vector<int>t(101,-1);
         int n=nums.size();
       return solve(nums,0,n,t);
+        
+    }
+};*/
+
+
+
+class Solution {
+public:
+
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+
+        if(n==0 ) return 0;
+
+        if(n==1) return nums[0];
+        
+        
+        vector<int>t(n+1,0);
+
+      //t[i] mtlb ith house tk kitna money stole kra hai
+
+      t[0]=0;
+      t[1]=nums[0];
+
+      for(int i=2;i<=n;i++){
+        int steal=nums[i-1]+t[i-2];
+        int skip=t[i-1];
+
+        t[i]=max(steal,skip);
+      }
+
+      return t[n];
+
+
+
         
     }
 };
